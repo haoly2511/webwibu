a43346169029af24e75a33145d468ae048ee31c7
haoly2511 <haoly456@gmail.com>
2022-06-27 18:46:24 +0700

    push code

---
+++ README.txt
@@ -0,0 +1,5 @@
+Thông tin thành viên:
+1. Nguyễn Quốc Tuấn - 3118410462
+2. Trần Quốc Bảo - 3118410027
+3. Phan Đặng Nhật Linh - 3118412033
+4. Đào Nguyễn Hoài Bảo - 3118410021
---
+++ application/views/frontend/modules/footer.php
@@ -0,0 +1,59 @@
+<footer id="footer">
+    <div class="news-social">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <ul class="list-unstyled social pull-right">
+                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                  <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+              </ul>
+          </div>
+      </div>
+  </div>
+  <div class="top-footer">
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <div class="col-xs-12 col-sm-6">
+                <a href="trang-chu">
+                    <img src="public/images/wibu2.png" style="width: 100%;" class="img-fix f-logo" alt="smart-construction">
+                </a>
+                <div class="about-store">
+                 Shop Wibu B. Chất lượng - giá cả hợp lý
+             </div>
+         </div>
+         <div class="col-xs-12 col-sm-6">
+            <div class="f-contact">
+                <h3>Thông tin liên hệ</h3>
+                <ul class="list-unstyled">
+                    <li class="clearfix">
+                        <i class="fa fa-map-marker"></i>
+                        <span>33 Trần Xuân Soạn, Quận 7, TPHCM</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-phone"></i>
+                        <span>0978651616 - 0981.334455</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-envelope"></i>
+                        <span><a href="mailto:baovip0201@gmail.com">baovip0201@gmail.com</a></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6">
+        <div class="col-xs-12 col-sm-6">
+            <h3 class="footer-title">HỖ TRỢ KHÁCH HÀNG</h3>
+            <ul class="list-unstyled linklists">
+                <li><a href="chinh-sach">Hướng dẫn thanh toán</a></li>          
+                <li><a href="chinh-sach">Hướng dẫn đặt hàng</a></li>
+                <li><a href="dieu-khoan">Điều khoản</a></li>                   
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+       </div>
+   </div>
+</div>
+</div>
+</footer>
